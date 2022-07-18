e64e74f81967832ab2cde5d09da4ceb078597ffd
yannsecundo <yannsecundo34@gmail.com>
2022-07-18 05:56:46 -0400

    criação das props no form e conclusão do requisito 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 
 export class FormCard extends Component {
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      maxSumAttr,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
     return (
       <div>
         <h2>Adicionar Nova Carta</h2>
@@ -9,49 +24,87 @@ export class FormCard extends Component {
           <input
             type="text"
             data-testid="name-input"
+            value={ cardName }
+            onChange={ onInputChange }
           />
+
           <br />
+
           <input
             type="textarea"
             data-testid="description-input"
+            value={ cardDescription }
+            onChange={ onInputChange }
           />
+
           <br />
+
           <input
             type="number"
             data-testid="attr1-input"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
           />
+
           <br />
+
           <input
             type="number"
             data-testid="attr2-input"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
           />
+
           <br />
+
           <input
             type="number"
             data-testid="attr3-input"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
           />
+
           <br />
+
           <input
             type="text"
             data-testid="image-input"
+            value={ cardImage }
+            onChange={ onInputChange }
           />
+
           <br />
+
           <select
             data-testid="rare-input"
+            value={ cardRare }
+            onChange={ onInputChange }
           >
             <option value="normal">normal</option>
             <option value="raro">raro</option>
             <option value="muito raro">muito raro</option>
           </select>
+
           <br />
+
           <input
             type="checkbox"
             data-testid="trunfo-input"
+            checked={ cardTrunfo }
+            onChange={ onInputChange }
           />
+
           <br />
-          <button type="submit" data-testid="save-button">
+
+          <button
+            type="submit"
+            data-testid="save-button"
+            disabled={ isSaveButtonDisabled }
+            onClick={ onSaveButtonClick }
+          >
             Salvar
           </button>
+
         </form>
       </div>
     );
